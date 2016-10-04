621c74038b75bd46957c74627b89568b4d75e980
shenanigan911 <steve@refactoru.com>
2016-10-04 12:15:46 -0600

    remove __dirname in express.static

--- boilerplate/express/server.js
+++ boilerplate/express/server.js
@@ -10,7 +10,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('/public'));
 
 // Routes \\
 app.get('/', function(req, res){
